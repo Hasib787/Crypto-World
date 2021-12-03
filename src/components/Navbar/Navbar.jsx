9928a4ec58224ceb820984a0d1731d32ebff8c15
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-03 17:23:17 +0600

    added title and a avater on b=navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import {Link} from 'react-router-dom';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons';
+
+const Navbar = () => {
+    return (
+        <div className='nav-container'>
+            <div className="logo-container">
+                <Avatar/>
+                <Typography.Title level={2} className="logo">
+                    <Link to='/'></Link>
+                </Typography.Title>
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
